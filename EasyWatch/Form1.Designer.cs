b4dac090624b0745f4fbceba917df46252d70cf4
杨凌宇 <850759629@qq.com>
2019-08-26 21:42:25 +0800

    添加项目文件。

---
+++ EasyWatch/Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace EasyWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.编辑ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.视图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(74, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开串口";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(74, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Click += new System.EventHandler(this.comboBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(228, 94);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(543, 263);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(696, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "发送";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "串口号";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "串口状态：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(320, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "关闭";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.comboBox5);
+            this.groupBox1.Controls.Add(this.comboBox4);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(203, 370);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口设置";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(8, 266);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(52, 19);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hex";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(8, 238);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 19);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "ASCII";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(106, 238);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(89, 19);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "显示发送";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(106, 267);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "显示时间";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "1",
+            "1.5",
+            "2"});
+            this.comboBox5.Location = new System.Drawing.Point(74, 192);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 23);
+            this.comboBox5.TabIndex = 2;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "None",
+            "Odd",
+            "Even"});
+            this.comboBox4.Location = new System.Drawing.Point(74, 152);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 23);
+            this.comboBox4.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.comboBox3.Location = new System.Drawing.Point(74, 112);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 23);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(5, 195);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 15);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "停止位";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400"});
+            this.comboBox2.Location = new System.Drawing.Point(74, 72);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(5, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "校验位";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "数据位";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "波特率";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 379);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(462, 53);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(696, 54);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 31);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "清屏";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.编辑ToolStripMenuItem,
+            this.视图ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(790, 28);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 编辑ToolStripMenuItem
+            // 
+            this.编辑ToolStripMenuItem.Name = "编辑ToolStripMenuItem";
+            this.编辑ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.编辑ToolStripMenuItem.Text = "编辑";
+            // 
+            // 视图ToolStripMenuItem
+            // 
+            this.视图ToolStripMenuItem.Name = "视图ToolStripMenuItem";
+            this.视图ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.视图ToolStripMenuItem.Text = "视图";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 496);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "爽心版串口通讯  作者：杨凌宇";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 编辑ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 视图ToolStripMenuItem;
+    }
+}
+
---
+++ EasyWatch/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace EasyWatch
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serialPort = new SerialPort();
+        private delegate void SetTextCallback(string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //在给textBox1.text赋值的地方调用以下方法即可
+        private void SetText(string text)
+        {
+            // InvokeRequired需要比较调用线程ID和创建线程ID
+            // 如果它们不相同则返回true
+            if (this.textBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.textBox1.AppendText(text);
+            }
+        }
+
+        //选择串口
+        private void comboBox1_Click(object sender, EventArgs e)
+        {
+            if(serialPort.IsOpen)
+            {
+                MessageBox.Show("请先关闭当前串口");
+                return;
+            }
+            comboBox1.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox2.Text = comboBox2.Items[3].ToString(); //波特率
+            comboBox3.Text = comboBox3.Items[3].ToString(); //数据位
+            comboBox4.Text = comboBox4.Items[0].ToString(); //校验位
+            comboBox5.Text = comboBox5.Items[0].ToString(); //停止位
+
+
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);   //注册串口接收事件，给事件的委托添加处理方法            
+        }
+
+        //串口接收事件处理方法
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs args)
+        {
+
+            if (radioButton2.Checked)
+            {
+                byte[] data = new byte[serialPort.BytesToRead];
+                serialPort.Read(data, 0, serialPort.BytesToRead);
+                if (checkBox1.Checked)
+                {
+                    SetText(DateTime.Now.ToString("[HH:mm:ss] "));  //显示时间
+                }
+                for (int i = 0; i < data.Length; i++)
+                {
+                    string str = Convert.ToString(data[i], 16).ToUpper();   //把byte以16进制的形式转化为字符串，并大写
+                    SetText(str.Length == 1 ? "0" + str : str);   //补0
+                    SetText(" ");
+                }
+            }
+            else if (radioButton1.Checked)
+            {
+                char[] data = new char[serialPort.BytesToRead];
+                serialPort.Read(data, 0, serialPort.BytesToRead);
+                if (checkBox1.Checked)
+                {
+                    SetText(DateTime.Now.ToString("[HH:mm:ss] "));  //显示时间
+                }
+                string str = new string(data);
+                SetText(str);          
+            }
+
+            SetText("\n");  //换行
+        }
+
+        //打开/关闭串口
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+                if (button1.Text == "打开串口")
+                {
+                    if (serialPort.IsOpen == false)
+                    {
+                        if(comboBox1.Text == "")
+                        {
+                        MessageBox.Show("请选择串口");
+                            return;
+                        }
+                        serialPort.PortName = comboBox1.Text;                        
+                        serialPort.BaudRate = Convert.ToInt32(comboBox2.Text);
+                        serialPort.DataBits = Convert.ToInt32(comboBox3.Text);
+                        switch (comboBox4.Text)
+                        {
+                            case "None":
+                                serialPort.Parity = Parity.None;
+                                break;
+                            case "Even":
+                                serialPort.Parity = Parity.Even;
+                                break;
+                            case "Odd":
+                                serialPort.Parity = Parity.Odd;
+                                break;
+                        }
+                        switch (comboBox5.Text)
+                        {
+                            case "1":
+                                serialPort.StopBits = StopBits.One;
+                                break;
+                            case "2":
+                                serialPort.StopBits = StopBits.Two;
+                                break;
+                            case "1.5":
+                                serialPort.StopBits = StopBits.OnePointFive;
+                                break;
+                        }
+
+                        serialPort.ReadBufferSize = 10 * 1024;
+
+                    try
+                    {
+                        serialPort.Open();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                        return;
+                    }
+
+                    button1.Text = "关闭串口";
+                    label3.Text = "开启";
+
+
+                    }
+                }
+                else
+                {
+                    if (serialPort.IsOpen)
+                    {
+                        serialPort.Close();
+                    }
+                    button1.Text = "打开串口";
+                    label3.Text = "关闭";
+                }
+         
+        }
+
+
+        //串口发送
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen == false)
+            {
+                MessageBox.Show("串口未开启");
+                return;
+            }
+            if (radioButton2.Checked) //Hex发送
+            {
+                string str = textBox2.Text;
+                string[] strs = str.Split(' ');
+                byte[] data = new byte[strs.Length];
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    data[i] = Convert.ToByte(strs[i].Substring(0, 2), 16);
+                }
+                if (checkBox2.Checked)  //显示发送
+                {
+                    if (checkBox1.Checked) { SetText(DateTime.Now.ToString("[HH:mm:ss] ") + str.ToUpper() + '\n'); }    //显示时间
+                    else { SetText(str.ToUpper() + '\n'); } //不显示时间
+                }
+                try
+                {
+                    serialPort.Write(data, 0, strs.Length); //写数据到串口}
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+            }
+            else if (radioButton1.Checked) //ASCII发送
+            {
+                string str = textBox2.Text;
+
+                if (checkBox2.Checked)  //显示发送
+                {
+                    if (checkBox1.Checked) { SetText(DateTime.Now.ToString("[HH:mm:ss] ") + str + '\n'); }    //显示时间
+                    else { SetText(str + '\n'); } //不显示时间
+                }
+                try
+                {
+                    serialPort.Write(str); //写数据到串口
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+
+            }
+
+
+        }
+        //窗口关闭时关闭串口并释放资源
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort.Close();
+            this.Dispose();
+        }
+
+        //清屏
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Clear();
+        }
+    }
+}
+
+
